d4ef4a59ed0c8098a968eb148d263214dd991f08
TenseiRyuuga <stijntjedelion@hotmail.com>
2017-12-20 14:29:54 +0100

    commiting correction to resopurces and recipes

--- src/main/java/SLRD/slrd/crafting/ModRecipes.java
+++ src/main/java/SLRD/slrd/crafting/ModRecipes.java
@@ -8,21 +8,22 @@ import SLRD.slrd.block.ModBlocks;
 import SLRD.slrd.item.ModItems;
 
 public class ModRecipes {
-	
-	public static void addRecipes() {
-		//shaped crafting recipes
-		GameRegistry.addRecipe(new ItemStack(ModItems.tinyDeadifferium), "B#B", "###", "B#B", '#', ModItems.ingotDeadifferium,  'B', Items.BONE);
-		GameRegistry.addRecipe(new ItemStack(ModItems.Jen), "###", "###", "###", '#', ModItems.tinyDeadifferium);
-		GameRegistry.addRecipe(new ItemStack(ModItems.DJJen), "###", "#M#", "###", '#', ModItems.tinyDeadifferium, 'M', Items.RECORD_CAT);
-		GameRegistry.addRecipe(new ItemStack(ModItems.RIPJen), "###", "#B#", "###", '#', ModItems.tinyDeadifferium,  'B', Items.BONE);
-		GameRegistry.addRecipe(new ItemStack(ModItems.LOVEJen), "###", "#D#", "###", '#', ModItems.tinyDeadifferium, 'D', Blocks.RED_FLOWER);
-		
-		//shapeless crafting recipes
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tinyDeadifferium, 9), new ItemStack(ModItems.Jen));
-		
-		//smelting recipes
-		GameRegistry.addSmelting(ModBlocks.oreDeadifferium, new ItemStack(ModItems.ingotDeadifferium), 1.0f);
-		GameRegistry.addSmelting(new ItemStack(ModItems.tinyDeadifferium, 9), new ItemStack(ModItems.Jen), 1.0f);
-		
-	}
+
+    public static void addRecipes() {
+        //shaped crafting recipes
+        GameRegistry.addRecipe(new ItemStack(ModItems.tinyDeadifferium), "B#B", "###", "B#B", '#', ModItems.ingotDeadifferium, 'B', Items.BONE);
+        GameRegistry.addRecipe(new ItemStack(ModItems.Jen), "###", "###", "###", '#', ModItems.tinyDeadifferium);
+        GameRegistry.addRecipe(new ItemStack(ModItems.DJJen), "###", "#M#", "###", '#', ModItems.tinyDeadifferium, 'M', Items.RECORD_CAT);
+        GameRegistry.addRecipe(new ItemStack(ModItems.RIPJen), "###", "#B#", "###", '#', ModItems.tinyDeadifferium, 'B', Items.BONE);
+        GameRegistry.addRecipe(new ItemStack(ModItems.LOVEJen), "###", "#D#", "###", '#', ModItems.tinyDeadifferium, 'D', Blocks.RED_FLOWER);
+        GameRegistry.addRecipe(new ItemStack(ModItems.DeadifferTeeHeePickaxe), " S ", " S ", "DDD", 'D', ModItems.ingotDeadifferium, 'S', Items.STICK);
+
+        //shapeless crafting recipes
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tinyDeadifferium, 9), new ItemStack(ModItems.Jen));
+     
+        //smelting recipes
+        GameRegistry.addSmelting(ModBlocks.oreDeadifferium, new ItemStack(ModItems.ingotDeadifferium), 1.0f);
+        GameRegistry.addSmelting(new ItemStack(ModItems.tinyDeadifferium, 8), new ItemStack(ModItems.Jen), 1.0f);
+
+    }
 }
