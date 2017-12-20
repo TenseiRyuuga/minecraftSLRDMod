17137c458f1de2e7bb3dc035e616ad5fde5e9826
TenseiRyuuga <stijntjedelion@hotmail.com>
2017-12-20 14:32:42 +0100

    commiting recipe change and adding nb gradle properties

--- src/main/java/SLRD/slrd/crafting/ModRecipes.java
+++ src/main/java/SLRD/slrd/crafting/ModRecipes.java
@@ -16,14 +16,14 @@ public class ModRecipes {
         GameRegistry.addRecipe(new ItemStack(ModItems.DJJen), "###", "#M#", "###", '#', ModItems.tinyDeadifferium, 'M', Items.RECORD_CAT);
         GameRegistry.addRecipe(new ItemStack(ModItems.RIPJen), "###", "#B#", "###", '#', ModItems.tinyDeadifferium, 'B', Items.BONE);
         GameRegistry.addRecipe(new ItemStack(ModItems.LOVEJen), "###", "#D#", "###", '#', ModItems.tinyDeadifferium, 'D', Blocks.RED_FLOWER);
-        GameRegistry.addRecipe(new ItemStack(ModItems.DeadifferTeeHeePickaxe), " S ", " S ", "DDD", 'D', ModItems.ingotDeadifferium, 'S', Items.STICK);
+        GameRegistry.addRecipe(new ItemStack(ModItems.DeadifferTeeHeePickaxe), "SSS", "SSS", "DDD", 'D', ModItems.ingotDeadifferium, 'S', Items.STICK);
 
         //shapeless crafting recipes
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tinyDeadifferium, 9), new ItemStack(ModItems.Jen));
      
         //smelting recipes
         GameRegistry.addSmelting(ModBlocks.oreDeadifferium, new ItemStack(ModItems.ingotDeadifferium), 1.0f);
-        GameRegistry.addSmelting(new ItemStack(ModItems.tinyDeadifferium, 8), new ItemStack(ModItems.Jen), 1.0f);
+        GameRegistry.addSmelting(new ItemStack(ModItems.tinyDeadifferium, 9), new ItemStack(ModItems.Jen), 1.0f);
 
     }
 }
